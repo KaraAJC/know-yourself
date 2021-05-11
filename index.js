12de8a828a7a8b7425bf813e12b9fb4460480c3d
KaraAJC <KaraAJC@gmail.com>
2021-05-11 17:16:45 -0500

    Create json bin

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 function dataLoad() {
-  const dataUrl = "#"
-
-  // load file
-  // fetch("data.json").then(data => console.log(data))
-  // fetch('data.json', mode: 'no-cors')
-  // .then(response => response.json())
-  //   .then(data => console.log(data))
-  //   .catch(error => console.log(error));
-  fetch('https://api.github.com/users/{YOUR_USERNAME}')
+  // edit the API data at https://www.npoint.io/docs/9787612add7f474247e4
+  fetch('https://api.npoint.io/9787612add7f474247e4')
     .then(response => response.json())
+      .then(data => console.log(data))
     .catch( error => console.error(error));
 }
 
